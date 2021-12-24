028fd1364795aad9ace3e1912568335815e5a44b
gbudjeakp <gbudjeakp@gmail.com>
2021-12-24 00:56:12 -0600

    adding delta time to code

--- Relearn Level Design Code/Assets/Mover.cs
+++ Relearn Level Design Code/Assets/Mover.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+   [SerializeField] int moveSpeed = 3;
     // Start is called before the first frame update
     void Start()
     {
-
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
 }
