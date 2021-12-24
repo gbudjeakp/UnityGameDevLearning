4d93e01e923d42a7e9a00618b5173a2e55511441
gbudjeakp <gbudjeakp@gmail.com>
2021-12-23 23:06:13 -0600

    updating code

--- Relearn Level Design Code/Assets/Mover.cs
+++ Relearn Level Design Code/Assets/Mover.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     // Start is called before the first frame update
-
-   [SerializeField] float x = 0;
-    [SerializeField] float y = 0f;
-   [SerializeField] float z = 0f;
     void Start()
     {
-      transform.Translate(x, y, z);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(x, y, z);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue);
     }
 }
