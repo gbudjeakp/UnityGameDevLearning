dd7d92f16e891b7fed53aa148eb2ea0fd3e3c1e7
gbudjeakp <gbudjeakp@gmail.com>
2021-12-24 18:40:15 -0600

    practicing methods

--- Relearn Level Design Code/Assets/Mover.cs
+++ Relearn Level Design Code/Assets/Mover.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
-   [SerializeField] int moveSpeed = 3;
+    [SerializeField] int moveSpeed = 3;
     // Start is called before the first frame update
     void Start()
     {
-      
+     Greeter();
     }
 
     // Update is called once per frame
@@ -19,4 +19,13 @@ public class Mover : MonoBehaviour
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
+
+    //Testing Methods
+    void Greeter()
+    {
+        Debug.Log("Welcome to Kubaz.");
+        Debug.Log("Move your player with AWSD");
+        Debug.Log("Walls are dangerous");
+    }
+
 }
