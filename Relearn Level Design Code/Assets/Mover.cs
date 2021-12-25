11760e94c4dbe1d4869262dd4b145517ac361c63
gbudjeakp <gbudjeakp@gmail.com>
2021-12-24 18:58:07 -0600

    practicing methods

--- Relearn Level Design Code/Assets/Mover.cs
+++ Relearn Level Design Code/Assets/Mover.cs
@@ -9,23 +9,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     Greeter();
+      Greeter();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    //Practicing Methods 
+    void Greeter(){
+        Debug.Log("Welcome to Kubaz");
+        Debug.Log("Our controls are ASWD");
+        Debug.Log("Do not hit the Walls");
+    }
+    void Movement()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
-    }
-
-    //Testing Methods
-    void Greeter()
-    {
-        Debug.Log("Welcome to Kubaz.");
-        Debug.Log("Move your player with AWSD");
-        Debug.Log("Walls are dangerous");
     }
 
 }
