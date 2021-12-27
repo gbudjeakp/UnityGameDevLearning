be138d7d2fc0daa7020889732eb1f8ee3d82ad19
gbudjeakp <gbudjeakp@gmail.com>
2021-12-27 13:41:11 -0600

    Adding switch statement in collision scripts

--- Rocketz/Assets/Scripts/CollisionHandler.cs
+++ Rocketz/Assets/Scripts/CollisionHandler.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void Start()
+    private void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    void Update()
-    {
-        
+        switch (other.gameObject.tag)
+        {
+            case "Fuel":
+                Debug.Log("Got more Fuel");
+                break;
+            case "Friendly":
+                Debug.Log("On Launch Pad");
+                break;
+            case "Finish":
+                Debug.Log("Landing Successful");
+                break;
+            default:
+                Debug.Log("Player Died");
+                break;
+        }
     }
 }
