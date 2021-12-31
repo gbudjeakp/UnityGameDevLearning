9d448ee938c346b162de0f16e563deac4dc04971
gbudjeakp <gbudjeakp@gmail.com>
2021-12-31 16:02:51 -0600

    added quit

---
+++ Rocketz/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        quitGame();
+    }
+
+    void quitGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Game Ended");
+            Application.Quit();
+
+        }
+    }
+}
