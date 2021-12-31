8cfcc20ae1cf20d27a6c211a177c260eefa43def
gbudjeakp <gbudjeakp@gmail.com>
2021-12-31 15:26:34 -0600

    adding epsilon

--- Rocketz/Assets/Scripts/Oscillator.cs
+++ Rocketz/Assets/Scripts/Oscillator.cs
@@ -17,6 +17,10 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2;
         float rawSignWave = Mathf.Sin(cycles * tau);
