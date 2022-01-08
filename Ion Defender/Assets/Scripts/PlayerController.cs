e540b48a6df726dbb0a8ea8a159e3100a4f27716
gbudjeakp <gbudjeakp@gmail.com>
2022-01-08 15:35:18 -0600

    added assets and built terrain. Also added simple testing movement script

---
+++ Ion Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        float verticakThrow = Input.GetAxis("Vertical");
+        Debug.Log(horizontalThrow);
+        Debug.Log(verticakThrow);
+    }
+}
