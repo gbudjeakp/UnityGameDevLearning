d8ded28138acaca23eb5b3d81d5dc2c86e375271
gbudjeakp <gbudjeakp@gmail.com>
2022-01-08 21:00:57 -0600

    trying out the new input system

--- Ion Defender/Assets/Scripts/PlayerController.cs
+++ Ion Defender/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    [SerializeField] InputAction fire;
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
+     private void OnEnable() {
+         movement.Enable();
+     }
+
+     private void OnDisable() {
+         movement.Disable();
+     }
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticakThrow = Input.GetAxis("Vertical");
+          float horizontalThrow = movement.ReadValue<Vector2>().x;
+          float verticalThrow = movement.ReadValue<Vector2>().y;
+
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // float verticakThrow = Input.GetAxis("Vertical");
         Debug.Log(horizontalThrow);
-        Debug.Log(verticakThrow);
+        Debug.Log(verticalThrow);
+
     }
 }
