38005a1c4a045963de8b09f952f7fc60d8330f84
gbudjeakp <gbudjeakp@gmail.com>
2022-01-08 21:15:39 -0600

    clean up

--- Ion Defender/Assets/Scripts/PlayerController.cs
+++ Ion Defender/Assets/Scripts/PlayerController.cs
@@ -1,35 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
-    [SerializeField] InputAction fire;
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-     private void OnEnable() {
-         movement.Enable();
-     }
-
-     private void OnDisable() {
-         movement.Disable();
-     }
     // Update is called once per frame
     void Update()
     {
-          float horizontalThrow = movement.ReadValue<Vector2>().x;
-          float verticalThrow = movement.ReadValue<Vector2>().y;
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
 
-        // float horizontalThrow = Input.GetAxis("Horizontal");
-        // float verticakThrow = Input.GetAxis("Vertical");
-        Debug.Log(horizontalThrow);
-        Debug.Log(verticalThrow);
-
+        transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
     }
 }
