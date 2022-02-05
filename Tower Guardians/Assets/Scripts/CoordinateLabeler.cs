762efa59464a5b360e16e51a0b43c7d7f9d55a6a
gbudjeakp <gbudjeakp@gmail.com>
2022-02-05 12:35:43 -0600

    updating coordinates for tiles

--- Tower Guardians/Assets/Scripts/CoordinateLabeler.cs
+++ Tower Guardians/Assets/Scripts/CoordinateLabeler.cs
@@ -23,13 +23,19 @@ public class CoordinateLabeler : MonoBehaviour
         if (!Application.isPlaying)
         {
             DisplayCoordinates();
+            UpdateObjectName();
         }
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
     }
 
     private void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z);
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
         label.text = $"{coordinates.x}, {coordinates.y}";
     }
 }
