486e5e0c9666024d6b0fd720bd6919ded3865dae
gbudjeakp <gbudjeakp@gmail.com>
2022-02-06 14:57:24 -0600

    updated coroutines to use linear interpolation

---
+++ Tower Guardians/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            //Helps our enemy face the direction its looking at 
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1){
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+        }
+    }
+}
