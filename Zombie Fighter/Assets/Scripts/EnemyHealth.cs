2bb3a5234401d5d6b65135d7c441220efc489e43
gbudjeakp <gbudjeakp@gmail.com>
2022-04-17 12:47:29 -0500

    add particle effect for muzzle

---
+++ Zombie Fighter/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    //Creae a public method which rduces hitpoints by the amount of damage
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Zombie Fighter/Assets/Scripts/Weapon.cs
+++ Zombie Fighter/Assets/Scripts/Weapon.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] float range = 100f;
- 
+    [SerializeField] float weaponDamage = 10f;
+
     // Update is called once per frame
     void Update()
     {
@@ -19,8 +21,35 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        playMuzzleFlash();
+        ProcessRayCast();
+    }
+
+    private void playMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log(gameObject.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit:" + hit.transform.name);
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            //Add visual effect
+            //method to decrease enemy health
+            if (target == null)
+            {
+                return;
+            }
+
+            target.TakeDamage(weaponDamage);
+        }
+        else
+        {
+            return;
+        }
     }
 }
