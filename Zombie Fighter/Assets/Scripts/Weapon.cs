cd6e150392cbae217a312a44117d3bc685f3f368
gbudjeakp <gbudjeakp@gmail.com>
2022-04-17 09:40:27 -0500

    adding raycst

---
+++ Zombie Fighter/Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        Debug.Log(gameObject.name);
+    }
+}
