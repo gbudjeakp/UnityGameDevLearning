bd511ed2eb07a0c502ffc105a13e5500d18f59b2
gbudjeakp <gbudjeakp@gmail.com>
2022-08-13 17:36:14 -0500

    Adding player health

---
+++ Zombie Fighter/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float playerHealth = 100f;
+
+
+public void PlayerDeath(float damage)
+    {
+        playerHealth -= damage;
+
+        if(playerHealth <= 0)
+        {
+            Debug.Log("Player has died");
+        }
+    }
+}
